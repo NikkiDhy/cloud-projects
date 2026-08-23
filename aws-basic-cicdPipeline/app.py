5ffd07d01ef45110a51499cabecc520289866153
Riya Dhyani <riyadhyani1403@gmail.com>
2026-08-23 21:01:51 +0530

    added buildspec.yml

--- aws-basic-cicdPipeline/app.py
+++ aws-basic-cicdPipeline/app.py
@@ -1,1 +1,11 @@
-# test
+#a simple python flask app to test the pipeline
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello, world!'
+
+if __name__ == '__main__':
+    app.run()
